8d54231820d67f07c68d7118e17a412db747aa6d
igorpy1997 <igor.guley@gmail.com>
2023-04-27 00:46:20 +0300

    Adding of second decorator

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-
+#first task
+import time
 class UnexpectedTypeException(Exception):
     pass
 
@@ -18,8 +19,24 @@ def expected_type(return_types):
     return dec_func
 
 
-@expected_type(int)
+#Second task
+def timer(time_of_working):
+    def dec_func(wrap_function):
+        def wrapper(*args, **kwargs):
+            start_time = time.time()
+            wrap_function(*args, **kwargs)
+            run_time = time.time() - start_time
+            return run_time < time_of_working
+        return wrapper
+    return dec_func
+
+
+
+
+#@expected_type(int)
+@timer(2)
 def add_numbers(a,b):
+    time.sleep(1)
     return a+b
 
 print(add_numbers(0.22, 0.33))
